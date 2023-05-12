63a1f4ef62b4e04245c4e634ef947fb7f2ac9ab0
MoussaGRICHE <grimoussa@gmail.com>
2023-05-12 09:03:34 +0200

    read camera function modification

--- track_with_cpp/inference/main.cpp
+++ track_with_cpp/inference/main.cpp
@@ -56,6 +56,14 @@ const std::vector<std::vector<unsigned int>> COLORS = {
 const std::vector<std::string> DISPALYED_CLASS_NAMES = {
 	"car", "motorcycle" };
 
+std::string gstreamer_pipeline (int capture_width, int capture_height, int display_width, int display_height, int framerate, int flip_method) {
+    return "nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)" + std::to_string(capture_width) + ", height=(int)" +
+           std::to_string(capture_height) + ", framerate=(fraction)" + std::to_string(framerate) +
+           "/1 ! nvvidconv flip-method=" + std::to_string(flip_method) + " ! video/x-raw, width=(int)" + std::to_string(display_width) + ", height=(int)" +
+           std::to_string(display_height) + ", format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink";
+}
+
+
 int main(int argc, char** argv)
 {
     const std::string engine_file_path{ argv[1] };
@@ -122,7 +130,22 @@ int main(int argc, char** argv)
         }
         else
         {
-            cap.open(0);
+            int capture_width = 1280 ;
+			int capture_height = 720 ;
+			int display_width = 1280 ;
+			int display_height = 720 ;
+			int framerate = 30 ;
+			int flip_method = 0 ;
+
+			std::string pipeline = gstreamer_pipeline(capture_width,
+			capture_height,
+			display_width,
+			display_height,
+			framerate,
+			flip_method);
+			std::cout << "Using pipeline: \n\t" << pipeline << "\n";
+		
+			cv::VideoCapture cap(pipeline, cv::CAP_GSTREAMER);
         }
 
         if (!cap.isOpened())
