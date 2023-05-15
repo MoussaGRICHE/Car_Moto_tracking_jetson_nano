384a4f6ed88364f199c465b4f63707cc8e777716
MoussaGRICHE <grimoussa@gmail.com>
2023-05-15 15:00:28 +0200

    main.cpp modif

--- detection/c++/inference/main.cpp
+++ detection/c++/inference/main.cpp
@@ -66,143 +66,148 @@ std::string gstreamer_pipeline (int capture_width, int capture_height, int displ
 
 int main(int argc, char** argv)
 {
- const std::string engine_file_path{ argv[1] };
- const std::string input_type{ argv[2] };
- std::string input_value;
- int infer_rate;
- std::string output_type;
+    const std::string engine_file_path{ argv[1] };
+    const std::string input_type{ argv[2] };
+    std::string input_value;
+    int infer_rate;
+    std::string output_type;
 
- std::vector<std::string> imagePathList;
- bool isVideo{ false };
- bool isCamera{ false };
+    std::vector<std::string> imagePathList;
+    bool isVideo{ false };
+    bool isCamera{ false };
 
- auto yolov8 = new YOLOv8(engine_file_path);
- yolov8->make_pipe(true);
+    auto yolov8 = new YOLOv8(engine_file_path);
+    yolov8->make_pipe(true);
 
- if (input_type == "video")
- {
- assert(argc == 6);
- input_value = argv[3];
- infer_rate = std::stoi(argv[4]);
- output_type = argv[5];
- if (IsFile(input_value))
- {
- std::string suffix = input_value.substr(input_value.find_last_of('.') + 1);
- if (
- suffix == "mp4" ||
- suffix == "avi" ||
- suffix == "m4v" ||
- suffix == "mpeg" ||
- suffix == "mov" ||
- suffix == "mkv"
- )
- {
- isVideo = true;
- }
- else
- {
- printf("suffix %s is wrong !!!\n", suffix.c_str());
- std::abort();
- }
- }
- }
+    if (input_type == "video")
+    {
+        assert(argc == 6);
+        input_value = argv[3];
+        infer_rate = std::stoi(argv[4]);
+        output_type = argv[5];
+        if (IsFile(input_value))
+        {
+            std::string suffix = input_value.substr(input_value.find_last_of('.') + 1);
+            if (
+                suffix == "mp4" ||
+                suffix == "avi" ||
+                suffix == "m4v" ||
+                suffix == "mpeg" ||
+                suffix == "mov" ||
+                suffix == "mkv"
+                )
+            {
+                isVideo = true;
+            }
+            else
+            {
+                printf("suffix %s is wrong !!!\n", suffix.c_str());
+                std::abort();
+            }
+        }
+    }
 
- else if (input_type == "camera")
- {
- assert(argc == 5);
- infer_rate = std::stoi(argv[3]);
- output_type = argv[4];
- isCamera = true;
- }
+    else if (input_type == "camera")
+    {
+        assert(argc == 5);
+        infer_rate = std::stoi(argv[3]);
+        output_type = argv[4];
+        isCamera = true;
+    }
 
- cv::VideoCapture cap;
- cv::VideoWriter writer;
- if (isVideo)
- {
- cap.open(input_value);
- if (!cap.isOpened())
- {
- printf("can not open %s\n", input_value.c_str());
- return -1;
- }
- double fps = cap.get(cv::CAP_PROP_FPS);
- cv::Size size = cv::Size((int)cap.get(cv::CAP_PROP_FRAME_WIDTH), (int)cap.get(cv::CAP_PROP_FRAME_HEIGHT));
- if (output_type == "save") {
- writer.open("output.mp4", cv::VideoWriter::fourcc('m', 'p', '4', 'v'), 30, size);
- }
- }
- else
- {
- int capture_width = 1280;
- int capture_height = 720;
- int display_width = 1280;
- int display_height = 720;
- int framerate = 30;
- int flip_method = 2;
+    cv::VideoCapture cap;
+    cv::VideoWriter writer;
+    if (isVideo)
+    {
+        cap.open(input_value);
+        if (!cap.isOpened())
+        {
+            printf("can not open %s\n", input_value.c_str());
+            return -1;
+        }
+        double fps = cap.get(cv::CAP_PROP_FPS);
+        cv::Size size = cv::Size((int)cap.get(cv::CAP_PROP_FRAME_WIDTH), (int)cap.get(cv::CAP_PROP_FRAME_HEIGHT));
+        if (output_type == "save") {
+            writer.open("output.mp4", cv::VideoWriter::fourcc('m', 'p', '4', 'v'), 30, size);
+        }
+    }
+    else
+    {
+        int capture_width = 1280;
+        int capture_height = 720;
+        int display_width = 1280;
+        int display_height = 720;
+        int framerate = 30;
+        int flip_method = 2;
 
- std::string pipeline = gstreamer_pipeline(capture_width,
- capture_height,
- display_width,
- display_height,
- framerate,
- flip_method);
- std::cout << "Using pipeline: \n\t" << pipeline << "\n";
+        std::string pipeline = gstreamer_pipeline(capture_width,
+            capture_height,
+            display_width,
+            display_height,
+            framerate,
+            flip_method);
+        std::cout << "Using pipeline: \n\t" << pipeline << "\n";
 
- cap.open(pipeline, cv::CAP_GSTREAMER);
- if (!cap.isOpened()) {
- std::cout << "Failed to open camera." << std::endl;
- return (-1);
- }
- }
+        cap.open(pipeline, cv::CAP_GSTREAMER);
+        if (!cap.isOpened()) {
+            std::cout << "Failed to open camera." << std::endl;
+            return (-1);
+        }
 
- cv::Mat res, image;
- cv::Size size = cv::Size{ 640, 640 };
- std::vector<Object> objs;
+		if (output_type == "save") {
+           cv::Size size = cv::Size((int)cap.get(cv::CAP_PROP_FRAME_WIDTH), (int)cap.get(cv::CAP_PROP_FRAME_HEIGHT));
+           writer.open("output.mp4", cv::VideoWriter::fourcc('m', 'p', '4', 'v'), 30, size);
+       }
+    }
 
- cv::namedWindow("result", cv::WINDOW_AUTOSIZE);
+	cv::Mat res, image;
+	cv::Size size = cv::Size{ 640, 640 };
+	std::vector<Object> objs;
 
- int frame_count = 0;
+	cv::namedWindow("result", cv::WINDOW_AUTOSIZE);
 
- while (cap.read(image))
- {
- objs.clear();
- yolov8->copy_from_Mat(image, size);
- auto start = std::chrono::system_clock::now();
+	int frame_count = 0;
 
- if (frame_count % infer_rate == 0)
- {
- yolov8->infer();
- yolov8->postprocess(objs);
- }
+	while (cap.read(image))
+	{
+	objs.clear();
+	yolov8->copy_from_Mat(image, size);
+	auto start = std::chrono::system_clock::now();
 
- auto end = std::chrono::system_clock::now();
- yolov8->draw_objects(image, res, objs, CLASS_NAMES, COLORS, DISPALYED_CLASS_NAMES);
+	if (frame_count % infer_rate == 0)
+	{
+	yolov8->infer();
+	yolov8->postprocess(objs);
+	}
 
- if (output_type == "save") {
- writer.write(res);
- }
+	auto end = std::chrono::system_clock::now();
+	yolov8->draw_objects(image, res, objs, CLASS_NAMES, COLORS, DISPALYED_CLASS_NAMES);
 
- auto tc = (double)
- std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() / 1000.;
- double fps = 1000 / tc;
+	if (output_type == "save") {
+	writer.write(res);
+	}
 
- printf("cost %2.4lf ms (%2.4lf fps)\n", tc, fps);
+	auto tc = (double)
+	std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() / 1000.;
+	double fps = 1000 / tc;
+
+	printf("cost %2.4lf ms (%2.4lf fps)\n", tc, fps);
 
 
- if(output_type=="show"){
- cv::imshow("result", res);
- if (cv::waitKey(10) == 'q')
- {
- break;
- }
- }
+	if(output_type=="show"){
+	cv::imshow("result", res);
+	if (cv::waitKey(10) == 'q')
+	{
+	break;
+	}
+	}
 
- frame_count++;
- }
+	frame_count++;
+	}
 
- cv::destroyAllWindows();
- delete yolov8;
- return 0;
+	cv::destroyAllWindows();
+	delete yolov8;
+	return 0;
 }
 
 
