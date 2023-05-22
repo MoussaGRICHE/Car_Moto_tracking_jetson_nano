a2f42e275787a7113fbdc580deb50b1cf15b23fa
MoussaGRICHE <grimoussa@gmail.com>
2023-05-22 10:18:09 +0200

    add infer_rate condition to bytetrack

--- tracking/bytetrack/src/main.cpp
+++ tracking/bytetrack/src/main.cpp
@@ -93,202 +93,195 @@ int main(int argc, char** argv)
     yolov8->make_pipe(true);
 
     if (input_type == "video")
-		{
-			assert(argc == 6);
-			input_value = argv[3];
-			infer_rate = std::stoi(argv[4]);
-			output_type = argv[5];
-			if (IsFile(input_value))
-				{
-					std::string suffix = input_value.substr(input_value.find_last_of('.') + 1);
-					if (suffix == "mp4" || suffix == "avi" || suffix == "m4v" || suffix == "mpeg" || suffix == "mov" || suffix == "mkv")
-						{
-							isVideo = true;
-						}
-					else
-						{
-							printf("suffix %s is wrong !!!\n", suffix.c_str());
-							std::abort();
-						}
-				}
-		}
+    {
+        assert(argc == 6);
+        input_value = argv[3];
+        infer_rate = std::stoi(argv[4]);
+        output_type = argv[5];
+        if (IsFile(input_value))
+        {
+            std::string suffix = input_value.substr(input_value.find_last_of('.') + 1);
+            if (suffix == "mp4" || suffix == "avi" || suffix == "m4v" || suffix == "mpeg" || suffix == "mov" || suffix == "mkv")
+            {
+                isVideo = true;
+            }
+            else
+            {
+                printf("suffix %s is wrong !!!\n", suffix.c_str());
+                std::abort();
+            }
+        }
+    }
 
     else if (input_type == "camera")
-		{
-			assert(argc == 5);
-			infer_rate = std::stoi(argv[3]);
-			output_type = argv[4];
-			isCamera = true;
-		}
+    {
+        assert(argc == 5);
+        infer_rate = std::stoi(argv[3]);
+        output_type = argv[4];
+        isCamera = true;
+    }
 
     cv::VideoCapture cap;
     cv::VideoWriter writer;
     if (isVideo)
-		{
-			cap.open(input_value);
-			if (!cap.isOpened())
-				{
-					printf("can not open %s\n", input_value.c_str());
-					return -1;
-				}
-			
-			cv::Size size = cv::Size((int)cap.get(cv::CAP_PROP_FRAME_WIDTH), (int)cap.get(cv::CAP_PROP_FRAME_HEIGHT));
-			if (output_type == "save") 
-				{
-					// Get current time
-					auto t = std::time(nullptr);
-					auto tm = *std::localtime(&t);
+    {
+        cap.open(input_value);
+        if (!cap.isOpened())
+        {
+            printf("can not open %s\n", input_value.c_str());
+            return -1;
+        }
 
-					// Format date and time
-					std::ostringstream oss;
-					oss << std::put_time(&tm, "%Y-%m-%d_%H-%M-%S");
-					auto str = oss.str();
+        cv::Size size = cv::Size((int)cap.get(cv::CAP_PROP_FRAME_WIDTH), (int)cap.get(cv::CAP_PROP_FRAME_HEIGHT));
+        if (output_type == "save")
+        {
+            // Get current time
+            auto t = std::time(nullptr);
+            auto tm = *std::localtime(&t);
 
-					// Get filename without extension
-					size_t lastindex = input_value.find_last_of("."); 
-					size_t lastSlash = input_value.find_last_of('/');
-					size_t lastDot = input_value.find_last_of('.');
-					std::string rawname = input_value.substr(lastSlash + 1, lastDot - lastSlash - 1);
+            // Format date and time
+            std::ostringstream oss;
+            oss << std::put_time(&tm, "%Y-%m-%d_%H-%M-%S");
+            auto str = oss.str();
 
-					// Construct new filename
-					std::string new_filename = rawname + "_detection_" + str + ".mp4";
+            // Get filename without extension
+            size_t lastindex = input_value.find_last_of(".");
+            size_t lastSlash = input_value.find_last_of('/');
+            size_t lastDot = input_value.find_last_of('.');
+            std::string rawname = input_value.substr(lastSlash + 1, lastDot - lastSlash - 1);
 
-					writer.open(new_filename, cv::VideoWriter::fourcc('m', 'p', '4', 'v'), 30, size);  
-				}
-		}
+            // Construct new filename
+            std::string new_filename = rawname + "_detection_" + str + ".mp4";
+
+            writer.open(new_filename, cv::VideoWriter::fourcc('m', 'p', '4', 'v'), 30, size);
+        }
+    }
     else
-		{
-			int capture_width = 1280;
-			int capture_height = 720;
-			int display_width = 1280;
-			int display_height = 720;
-			int framerate = 30;
-			int flip_method = 2;
+    {
+        int capture_width = 1280;
+        int capture_height = 720;
+        int display_width = 1280;
+        int display_height = 720;
+        int framerate = 30;
+        int flip_method = 2;
 
-			std::string pipeline = gstreamer_pipeline(capture_width,
-				capture_height,
-				display_width,
-				display_height,
-				framerate,
-				flip_method);
-			std::cout << "Using pipeline: \n\t" << pipeline << "\n";
+        std::string pipeline = gstreamer_pipeline(capture_width,
+            capture_height,
+            display_width,
+            display_height,
+            framerate,
+            flip_method);
+        std::cout << "Using pipeline: \n\t" << pipeline << "\n";
 
-			cap.open(pipeline, cv::CAP_GSTREAMER);
-			if (!cap.isOpened()) 
-				{
-					std::cout << "Failed to open camera." << std::endl;
-					return (-1);
-				}
+        cap.open(pipeline, cv::CAP_GSTREAMER);
+        if (!cap.isOpened())
+        {
+            std::cout << "Failed to open camera." << std::endl;
+            return (-1);
+        }
 
-			cv::Size size = cv::Size((int)cap.get(cv::CAP_PROP_FRAME_WIDTH), (int)cap.get(cv::CAP_PROP_FRAME_HEIGHT));
-			if (output_type == "save") 
-				{
-					// Get current time
-					auto t = std::time(nullptr);
-					auto tm = *std::localtime(&t);
+        cv::Size size = cv::Size((int)cap.get(cv::CAP_PROP_FRAME_WIDTH), (int)cap.get(cv::CAP_PROP_FRAME_HEIGHT));
+        if (output_type == "save")
+        {
+            // Get current time
+            auto t = std::time(nullptr);
+            auto tm = *std::localtime(&t);
 
-					// Format date and time
-					std::ostringstream oss;
-					oss << std::put_time(&tm, "%Y-%m-%d_%H-%M-%S");
-					auto str = oss.str();
+            // Format date and time
+            std::ostringstream oss;
+            oss << std::put_time(&tm, "%Y-%m-%d_%H-%M-%S");
+            auto str = oss.str();
 
-					// Get filename without extension
-					size_t lastindex = input_value.find_last_of("."); 
-					std::string rawname = input_value.substr(0, lastindex);
+            // Get filename without extension
+            size_t lastindex = input_value.find_last_of(".");
+            std::string rawname = input_value.substr(0, lastindex);
 
-					// Construct new filename
-					std::string new_filename = "Camera_detection_" + str + ".mp4";
+            // Construct new filename
+            std::string new_filename = "Camera_detection_" + str + ".mp4";
 
-					writer.open(new_filename, cv::VideoWriter::fourcc('m', 'p', '4', 'v'), 30, size);
-					
-				}
-		}
-	int fps = cap.get(CAP_PROP_FPS);
+            writer.open(new_filename, cv::VideoWriter::fourcc('m', 'p', '4', 'v'), 30, size);
 
-	cv::Mat res, image;
-	cv::Size size = cv::Size{ 640, 640 };
-	std::vector<Object> objs;
+        }
+    }
+    int fps = cap.get(cv::CAP_PROP_FPS);
 
-	BYTETracker tracker(fps, 30);
+    cv::Mat res, image;
+    cv::Size size = cv::Size{ 640, 640 };
+    std::vector<Object> objs;
+
+    BYTETracker tracker(fps, 30);
 
     int total_ms = 0;
 
-	int frame_count = 0;
+    int frame_count = 0;
+    int infer_frame_count = 0;  // Counter for frames processed for inference and tracking
 
-	std::vector<STrack> output_stracks;
+    while (cap.read(image))
+    {
+        if (infer_frame_count % infer_rate == 0)  // Perform inference and tracking only for selected frames
+        {
+            objs.clear();
+            yolov8->copy_from_Mat(image, size);
 
-	while (cap.read(image))
-	{
-		
-		objs.clear();
-		yolov8->copy_from_Mat(image, size);
+            auto start = std::chrono::system_clock::now();
 
-		auto start = std::chrono::system_clock::now();
+            yolov8->infer();
+            yolov8->postprocess(objs);
 
-		if (frame_count % infer_rate)
-		{
-			yolov8->infer();
-			yolov8->postprocess(objs);
-			vector<STrack> output_stracks = tracker.update(objs);
-		}
-		
-		
+            vector<STrack> output_stracks = tracker.update(objs);
 
-		for (int i = 0; i < output_stracks.size(); i++) {
-			vector<float> tlwh = output_stracks[i].tlwh;
-			Scalar s = tracker.get_color(output_stracks[i].track_id);
+            for (int i = 0; i < output_stracks.size(); i++) {
+                vector<float> tlwh = output_stracks[i].tlwh;
+                Scalar s = tracker.get_color(output_stracks[i].track_id);
 
-			// Create a new Object instance and assign the tracker ID
-			Object obj;
-			obj.rect = Rect(tlwh[0], tlwh[1], tlwh[2], tlwh[3]);
-			obj.tracker_id = output_stracks[i].track_id;
+                // Create a new Object instance and assign the tracker ID
+                Object obj;
+                obj.rect = Rect(tlwh[0], tlwh[1], tlwh[2], tlwh[3]);
+                obj.tracker_id = output_stracks[i].track_id;
 
-			// Add the updated Object instance to the objs vector
-			objs.push_back(obj);
-		}
-		
-		auto end = std::chrono::system_clock::now();
+                // Add the updated Object instance to the objs vector
+                objs.push_back(obj);
+            }
 
-		if (total_ms != 0) {
-			putText(image, format("fps: %d num: %d", frame_count * 1000000 / total_ms, output_stracks.size()),
-					Point(0, 30), 0, 0.6, Scalar(0, 0, 255), 2, LINE_AA);
-		}
+            auto end = std::chrono::system_clock::now();
 
+            if (total_ms != 0) {
+                putText(image, format("fps: %d num: %d", frame_count * 1000000 / total_ms, output_stracks.size()),
+                    Point(0, 30), 0, 0.6, Scalar(0, 0, 255), 2, LINE_AA);
+            }
 
-		
+            total_ms = total_ms + chrono::duration_cast<chrono::microseconds>(end - start).count();
 
-		total_ms = total_ms + chrono::duration_cast<chrono::microseconds>(end - start).count();
-		
-		yolov8->draw_objects(image, res, objs, CLASS_NAMES, COLORS, DISPALYED_CLASS_NAMES);
+            yolov8->draw_objects(image, res, objs, CLASS_NAMES, COLORS, DISPALYED_CLASS_NAMES);
 
-		if (output_type == "save") 
-			{
-				writer.write(res);
-			}
+            if (output_type == "save")
+            {
+                writer.write(res);
+            }
 
-		auto tc = (double)
-		std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() / 1000.;
-		double fps = 1000 / tc;
+            auto tc = (double)
+                std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() / 1000.;
+            double fps = 1000 / tc;
 
-		printf("cost %2.4lf ms (%0.0lf fps)\n", tc, std::round(fps));
+            printf("cost %2.4lf ms (%0.0lf fps)\n", tc, std::round(fps));
 
+            if (output_type == "show")
+            {
+                cv::namedWindow("result", cv::WINDOW_NORMAL | cv::WINDOW_GUI_EXPANDED);
+                cv::setWindowProperty("result", cv::WND_PROP_FULLSCREEN, cv::WINDOW_FULLSCREEN);
+                cv::imshow("result", res);
+                if (cv::waitKey(10) == 'q')
+                {
+                    break;
+                }
+            }
+        }
 
-		if(output_type=="show")
-		{
-		cv::namedWindow("result", cv::WINDOW_NORMAL | cv::WINDOW_GUI_EXPANDED);
-		cv::setWindowProperty("result", cv::WND_PROP_FULLSCREEN, cv::WINDOW_FULLSCREEN);
-		cv::imshow("result", res);
-		if (cv::waitKey(10) == 'q')
-			{
-			break;
-			}
-		}
+        frame_count++;
+        infer_frame_count++;
+    }
 
-		frame_count ++;
-		
-	}
-
-	cv::destroyAllWindows();
-	delete yolov8;
-	return 0;
+    cv::destroyAllWindows();
+    delete yolov8;
+    return 0;
 }
